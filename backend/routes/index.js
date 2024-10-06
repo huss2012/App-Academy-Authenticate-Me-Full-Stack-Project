eb205220f28695d4d801211b56fb6a64f970c65b
huss2012 <huss24940@gmail.com>
2024-10-06 22:35:46 +0300

    Setup & test the api route.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const apiRputer = require('./api');
 
 router.get('/hello/world', function (req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.send("Hello World!");
 });
-
+router.use('/api',apiRputer)
 
 module.exports = router
